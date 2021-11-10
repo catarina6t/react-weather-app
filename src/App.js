4dcdb4b92bbd912895bf96d3ac48afe7a5e2b23d
catarina6t <catarinatorres@live.com.pt>
2021-11-10 18:03:03 +0000

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from "react";
+
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Cate and is{" "}
-        <a
-          href="https://github.com/catarina6t/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Cate and is{" "}
+          <a
+            href="https://github.com/catarina6t/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
