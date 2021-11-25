2b766159339dd64b0d58ec9e83394008abe817a3
catarina6t <catarinatorres@live.com.pt>
2021-11-25 23:33:46 +0000

    Fix Forecast and removed the Fahrenheit Unit

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,12 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
+import { cleanup } from "@testing-library/react";
 
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+    
+  useEffect(() => {
+      setLoaded(false);
+  }, [props.coordinates]);
+
+    
   function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
@@ -16,7 +24,7 @@ export default function WeatherForecast(props) {
       <div className="WeatherForecast">
         <div className="row">
           {forecast.map(function (dailyForecast, index) {
-            if (index < 58u) {
+            if (index < 5) {
               return (
                 <div className="col" key={index}>
                   <WeatherForecastDay data={dailyForecast} />
